9fdbfe71f7959f9bbd18f69ec43e8cc395e7e436
zmfkzj <qlwlal@naver.com>
2021-07-29 15:18:57 +0900

    폰트 파일 설치 수정

--- setup.py
+++ setup.py
@@ -12,11 +12,12 @@ install_requires = \
 check_call("conda install -y --file requirements.txt".split())
 setuptools.setup(
     name="ddt",
-    version="0.0.2",
+    version="0.0.3",
     author="zmfkzj",
     author_email="qlwlal@naver.com",
     description="이미지에 detection 결과를 그리기 위한 모듈",
     packages=setuptools.find_packages(),
     install_requires=install_requires,
     python_requires=">=3.8",
+    include_package_data = True
 )
